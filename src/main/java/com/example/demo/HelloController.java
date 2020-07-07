dc7502390aab28f1598cadafddce1053a7441afe
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2020-07-07 10:46:40 +0200

    HelloController.java - hello

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,13 +2,22 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.ArrayList;
 
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Hello World!";
+	@RequestMapping("/hello")
+	public List<Hello> showHello() {
+		return this.getHello();
+	}
+	
+	private List<Hello> getHello() {
+		List<Hello> helloList = new ArrayList<Hello>();
+		helloList.add(new Hello(1, "Hello World 1!"));
+		helloList.add(new Hello(2, "Hello World 2!"));		
+		return helloList;
 	}
 
 }
