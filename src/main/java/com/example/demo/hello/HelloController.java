4f5f8b527175279079c493cb71f19e48e28d027d
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2020-07-08 11:49:48 +0200

    HelloController.java moved to hello folder

--- src/main/java/com/example/demo/hello/HelloController.java
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
