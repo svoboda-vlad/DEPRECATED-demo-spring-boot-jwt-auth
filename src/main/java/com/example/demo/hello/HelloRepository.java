556fcc01169d845fb95da5f794dbdf72b678426b
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2020-07-08 12:58:32 +0200

    HelloRepository.java - import fix

--- src/main/java/com/example/demo/hello/HelloRepository.java
+++ src/main/java/com/example/demo/hello/HelloRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.hello;
 
-import org.springframework.data.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
