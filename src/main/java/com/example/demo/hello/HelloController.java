aca4e049a54073b63fc5f2a84dbde7dcee0fc649
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2020-07-08 12:47:42 +0200

    HelloController.java - HelloRepository - GET, POST

--- src/main/java/com/example/demo/hello/HelloController.java
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -1,23 +1,32 @@
 package com.example.demo.hello;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 import java.util.List;
-import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Transactional
 @RestController
 public class HelloController {
-
-	@RequestMapping("/hello")
-	public List<Hello> showHello() {
-		return this.getHello();
-	}
 	
-	private List<Hello> getHello() {
-		List<Hello> helloList = new ArrayList<Hello>();
-		helloList.add(new Hello(1, "Hello World 1!"));
-		helloList.add(new Hello(2, "Hello World 2!"));		
-		return helloList;
-	}
+    private final HelloRepository helloRepository;
+
+    public HelloController(HelloRepository helloRepository) {
+        this.helloRepository = helloRepository;
+    }	
+
+    @GetMapping("/hello")
+    public List<Hello> getAllHellos() {
+        return helloRepository.findAll();
+    }
+	
+    @PostMapping("/hello")
+    public List<Hello> createHello(@Valid @RequestBody Hello hello) {
+        return helloRepository.save(hello);	
+    }
 
 }
