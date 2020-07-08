30dc4939a19f528b596fce965c86eecf4f60f4b6
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2020-07-08 12:57:11 +0200

    HelloRepository.java - JpaRepository

--- src/main/java/com/example/demo/hello/HelloRepository.java
+++ src/main/java/com/example/demo/hello/HelloRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.hello;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HelloRepository extends CrudRepository<Hello, Long> {
+public interface HelloRepository extends JpaRepository<Hello, Long> {
 
 }
