a2bffa16ce8094d0bf572300b05d06e481bf0892
Vladimir Svoboda <svoboda-vlad@seznam.cz>
2020-11-11 13:27:38 +0000

    liquibase added, h2 enabled

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().cors().and().authorizeRequests()
 		.antMatchers(HttpMethod.POST, "/login").permitAll()
+		.antMatchers("/h2-console/**").permitAll()		
 		.anyRequest().authenticated()
 		.and()
 		// Filter for the api/login requests
@@ -33,7 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		UsernamePasswordAuthenticationFilter.class)
 	    // Filter for other requests to check JWT in header
 	    .addFilterBefore(new AuthenticationFilter(),
-	    UsernamePasswordAuthenticationFilter.class);
+		UsernamePasswordAuthenticationFilter.class)
+		.headers().frameOptions().disable();
 	}	
 	
 	@Bean
