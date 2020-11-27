489787503b345e3b841a93356a7bebc9bbdec27c
Vladimir Svoboda <svoboda-vlad@seznam.cz>
2020-11-27 11:13:46 +0000

    generated secret key

--- src/main/java/com/example/demo/security/AuthenticationService.java
+++ src/main/java/com/example/demo/security/AuthenticationService.java
@@ -2,6 +2,7 @@ package com.example.demo.security;
 
 import static java.util.Collections.emptyList;
 
+import java.security.Key;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
@@ -14,17 +15,22 @@ import org.springframework.security.core.Authentication;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 public class AuthenticationService {
   static final long EXPIRY_MINS = 60L;
-  static final String SIGNINGKEY = "VeryLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongSecretKey";
+  // We need a signing key, so we'll create one just for this example. Usually
+  // the key would be read from your application configuration instead.
+  static final Key SIGNINGKEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
   static final String PREFIX = "Bearer";
 
   static public void addToken(HttpServletResponse res, String username) {
-	Date expirationDateTime = Date.from(LocalDateTime.now().plusMinutes(EXPIRY_MINS).atZone(ZoneId.systemDefault()).toInstant());
+	Date expirationDateTime = Date.from(
+			LocalDateTime.now().plusMinutes(EXPIRY_MINS).atZone(ZoneId.systemDefault()).toInstant()
+			);
     String JwtToken = Jwts.builder().setSubject(username)
         .setExpiration(expirationDateTime)
-        .signWith(SignatureAlgorithm.HS512, SIGNINGKEY)
+        .signWith(SIGNINGKEY)
         .compact();
     res.addHeader("Authorization", PREFIX + " " + JwtToken);
     res.addHeader("Access-Control-Expose-Headers", "Authorization");
@@ -33,14 +39,15 @@ public class AuthenticationService {
   static public Authentication getAuthentication(HttpServletRequest request) {
     String token = request.getHeader("Authorization");
     if (token != null) {
-      String user = Jwts.parser()
+      String username = Jwts.parserBuilder()
           .setSigningKey(SIGNINGKEY)
+          .build()
           .parseClaimsJws(token.replace(PREFIX, ""))
           .getBody()
           .getSubject();
 
-      if (user != null)
-        return new UsernamePasswordAuthenticationToken(user, null, emptyList());
+      if (username != null)
+        return new UsernamePasswordAuthenticationToken(username, null, emptyList());
     }
     return null;
   }
