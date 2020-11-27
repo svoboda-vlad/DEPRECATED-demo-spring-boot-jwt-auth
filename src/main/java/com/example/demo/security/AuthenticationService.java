8437b0b56017197bf4dd451038e1f3b0b33c63f0
Vladimir Svoboda <svoboda-vlad@seznam.cz>
2020-11-27 10:07:02 +0000

    expiration time local date

--- src/main/java/com/example/demo/security/AuthenticationService.java
+++ src/main/java/com/example/demo/security/AuthenticationService.java
@@ -1,24 +1,29 @@
 package com.example.demo.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
+import static java.util.Collections.emptyList;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
 
-import static java.util.Collections.emptyList;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 public class AuthenticationService {
-  static final long EXPIRATIONTIME = 864_000_00; // 1 day in milliseconds
+  static final long EXPIRY_MINS = 60L;
   static final String SIGNINGKEY = "VeryLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongSecretKey";
   static final String PREFIX = "Bearer";
 
   static public void addToken(HttpServletResponse res, String username) {
+	Date expirationDateTime = Date.from(LocalDateTime.now().plusMinutes(EXPIRY_MINS).atZone(ZoneId.systemDefault()).toInstant());
     String JwtToken = Jwts.builder().setSubject(username)
-        .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
+        .setExpiration(expirationDateTime)
         .signWith(SignatureAlgorithm.HS512, SIGNINGKEY)
         .compact();
     res.addHeader("Authorization", PREFIX + " " + JwtToken);
