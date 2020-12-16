cc40a96008dd20ffd47ba3e4e415847f73f80afd
Vladimir Svoboda <svoboda-vlad@seznam.cz>
2020-12-16 07:55:35 +0000

    temporary not fully working

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.demo.security.User;
+import com.example.demo.security.UserRepository;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +16,13 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner dataLoader(UserRepository userRepo, PasswordEncoder encoder) {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				userRepo.save(new User("108564931079495851483", "svoboda.vladimir1@gmail.com", "Vladimir", "Svoboda", "cs"));
+			}
+		};
+	}
 }
--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -6,15 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -31,17 +29,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private AuthenticationService authenticationService;	
+
+	@Autowired
+	private UserDetailsService userDetailsService;	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().cors().and().authorizeRequests()
-		.antMatchers(HttpMethod.POST, "/login", "/login-google").permitAll()
+		.antMatchers(HttpMethod.POST, "/login-google").permitAll()
 		.antMatchers("/h2-console/**", "/test").permitAll()		
 		.anyRequest().authenticated()
 		.and()
-		// Filter for the api/login requests
-		.addFilterBefore(new LoginFilter("/login", authenticationManager()), 
-		UsernamePasswordAuthenticationFilter.class)
 		// Filter for the api/login-google requests
 		.addFilterBefore(new LoginGoogleFilter("/login-google", authenticationManager(), googleTokenVerifier()), 
 		UsernamePasswordAuthenticationFilter.class)
@@ -54,21 +52,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	@Override
 	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withUsername("user")
-				.password(encoder().encode("password"))
-				.roles("USER")
-				.build();
-
-		UserDetails userGoogle =
-				 User.withUsername("108564931079495851483")
-					.password(encoder().encode(""))
-					.roles("USER")
-					.build();
-		
-		return new InMemoryUserDetailsManager(user, userGoogle);
+		return userDetailsService;
 	}
-	
+		
 	@Bean
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
