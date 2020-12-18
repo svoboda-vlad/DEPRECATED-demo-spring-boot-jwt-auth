e06a1b4d384f8a8008e54b6ecc72d7a7ef20d3fa
Vladimir Svoboda <svoboda-vlad@seznam.cz>
2020-12-18 07:52:43 +0000

    autowired removed

--- src/main/java/com/example/demo/security/UserService.java
+++ src/main/java/com/example/demo/security/UserService.java
@@ -1,6 +1,5 @@
 package com.example.demo.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,7 +10,6 @@ public class UserService implements UserDetailsService {
 
 	private UserRepository userRepo;
 
-	@Autowired
 	public UserService(UserRepository userRepo) {
 		this.userRepo = userRepo;
 	}
