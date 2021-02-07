4979cbb030226237c1acf2a70de217aca4d7144f
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2021-02-07 12:25:17 +0000

    test data + responseentity

--- src/main/java/com/example/demo/note/NoteController.java
+++ src/main/java/com/example/demo/note/NoteController.java
@@ -1,9 +1,12 @@
 package com.example.demo.note;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,15 +19,19 @@ import lombok.RequiredArgsConstructor;
 public class NoteController {
 	
     private final NoteRepository noteRepository;
+    private static final String NOTE_URL = "/note";
 
-    @GetMapping("/note")
-    public List<Note> getAllNotes() {
-        return noteRepository.findAll();
+    @GetMapping(NOTE_URL)
+    public ResponseEntity<List<Note>> getAllNotes() {
+        return ResponseEntity.ok(noteRepository.findAll());
     }
 	
-    @PostMapping("/note")
-    public Note createNote(@Valid @RequestBody Note note) {
-        return noteRepository.save(note);
+    @PostMapping(NOTE_URL)
+    public ResponseEntity<Note> createNote(@Valid @RequestBody Note note) throws URISyntaxException {
+        Note saved = noteRepository.save(note);
+        return ResponseEntity
+                .created(new URI(NOTE_URL + "/" + saved.getId()))
+                .body(saved);
     }
 
 }
