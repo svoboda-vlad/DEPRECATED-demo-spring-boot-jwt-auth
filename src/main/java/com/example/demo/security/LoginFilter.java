1ccfaf864c182dd60ff14effff4fdad0eeb20c10
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2021-05-16 00:57:59 +0200

    security config cleaned

--- src/main/java/com/example/demo/security/LoginFilter.java
+++ src/main/java/com/example/demo/security/LoginFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,28 +19,22 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class LoginFilter extends AbstractAuthenticationProcessingFilter {
-	
-	private final UserRepository userRepository;
 
-  public LoginFilter(String url, AuthenticationManager authManager, UserRepository userRepository) {
-    super(new AntPathRequestMatcher(url));
-    setAuthenticationManager(authManager);
-    this.userRepository = userRepository;
-  }
+	@Autowired
+	private UserRepository userRepository;
 
-  @Override
-  public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
+	public LoginFilter(AuthenticationManager authManager) {
+		super(new AntPathRequestMatcher("/login", "POST"));
+		this.setAuthenticationManager(authManager);
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
 			throws AuthenticationException, IOException {
-        User user = new ObjectMapper()
-        .readValue(req.getInputStream(), User.class);
-        return getAuthenticationManager().authenticate(
-          new UsernamePasswordAuthenticationToken(
-            user.getUsername(),
-            user.getPassword(),
-            Collections.emptyList()
-            )
-        );
-  }
+		User user = new ObjectMapper().readValue(req.getInputStream(), User.class);
+		return getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),
+				user.getPassword(), Collections.emptyList()));
+	}
 
 	@Override
 	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
