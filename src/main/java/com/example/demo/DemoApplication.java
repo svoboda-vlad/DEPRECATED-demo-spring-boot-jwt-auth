6f04fda8a5f58a070859ebeaaffce6e8ad5f95a7
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2021-05-17 13:14:40 +0000

    dev profile added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.example.demo.exchangerate.CurrencyCode;
@@ -30,6 +31,7 @@ public class DemoApplication {
 	}
 	
 	@Bean
+	@Profile("dev")
 	public CommandLineRunner dataLoader(CurrencyCodeRepository currencyCodeRepo, UserRepository userRepo, PasswordEncoder encoder) {
 		return new CommandLineRunner() {
 			@Override
