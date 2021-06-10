0bb0c2f80d49fd09ae894632d3ec381f38e2a2c1
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2021-06-10 08:27:42 +0000

    another integ tests added

---
+++ src/test/java/com/example/demo/google/GoogleLoginFilterIntegTest.java
@@ -0,0 +1,90 @@
+package com.example.demo.google;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.security.User;
+import com.example.demo.security.User.LoginProvider;
+import com.example.demo.security.UserRepository;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.json.webtoken.JsonWebSignature.Header;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+//@WithMockUser - not needed
+class GoogleLoginFilterIntegTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@MockBean
+	private GoogleIdTokenVerifier googleIdTokenVerifier;
+	
+	@BeforeEach
+	void initData() {
+		User user = new User("user321", encoder.encode(""),LoginProvider.GOOGLE);
+		userRepository.save(user);
+	}
+
+	@Test
+	void testGoogleLoginOk200() throws Exception {
+		String requestUrl = "/google-login";
+		String requestJson = "{\"idToken\":\"abcdef\"}";
+		int expectedStatus = 200;
+		String expectedJson = "";
+		
+		Header header = new Header();
+		Payload payload = new Payload();
+		payload.setSubject("user321");
+		GoogleIdToken idToken = new GoogleIdToken(header, payload, new byte[0], new byte[0]);		
+		given(googleIdTokenVerifier.verify("abcdef")).willReturn(idToken);
+				
+		this.mvc.perform(post(requestUrl).content(requestJson).contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().is(expectedStatus))
+				.andExpect(content().string(expectedJson))
+				.andExpect(header().exists("Authorization"));
+	}
+	
+	@Test
+	void testGoogleLoginRegisterNewUserOk200() throws Exception {
+		String requestUrl = "/google-login";
+		String requestJson = "{\"idToken\":\"abcdef\"}";
+		int expectedStatus = 200;
+		String expectedJson = "";
+		
+		Header header = new Header();
+		Payload payload = new Payload();
+		payload.setSubject("user322");
+		GoogleIdToken idToken = new GoogleIdToken(header, payload, new byte[0], new byte[0]);
+		given(googleIdTokenVerifier.verify("abcdef")).willReturn(idToken);
+		
+		this.mvc.perform(post(requestUrl).content(requestJson).contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().is(expectedStatus))
+				.andExpect(content().string(expectedJson))
+				.andExpect(header().exists("Authorization"));
+	}
+	
+}
--- src/test/java/com/example/demo/security/RegistrationControllerIntegTest.java
+++ src/test/java/com/example/demo/security/RegistrationControllerIntegTest.java
@@ -1,24 +1,25 @@
 package com.example.demo.security;
 
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.security.User.LoginProvider;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class RegistrationControllerTest {
+@Transactional
+class RegistrationControllerIntegTest {
 	
 	@Autowired
 	private MockMvc mvc;
@@ -26,8 +27,14 @@ class RegistrationControllerTest {
 	@Autowired
 	private PasswordEncoder encoder;	
 	
-	@MockBean
+	@Autowired
 	private UserRepository userRepository;
+	
+	@BeforeEach
+	void initData() {
+		User user = new User("user321", encoder.encode("pass321"),LoginProvider.INTERNAL);
+		userRepository.save(user);
+	}	
 	
 	@Test
 	void testRegisterUserCreated201() throws Exception {
@@ -44,13 +51,10 @@ class RegistrationControllerTest {
 	@Test
 	void testRegisterUserExistsAlreadyBadRequest400() throws Exception {
 		String requestUrl = "/register";
-		String requestJson = "{\"username\": \"test1\", \"password\": \"test123\"}";
+		String requestJson = "{\"username\": \"user321\", \"password\": \"test123\"}";
 		int expectedStatus = 400;
 		String expectedJson = "";
-		
-		User user = new User("test1",encoder.encode("test123"),LoginProvider.INTERNAL);
-		given(userRepository.findByUsername("test1")).willReturn(user);
-										
+												
 		this.mvc.perform(post(requestUrl).content(requestJson).contentType(MediaType.APPLICATION_JSON))
 		.andExpect(status().is(expectedStatus))
 				.andExpect(content().string(expectedJson));
