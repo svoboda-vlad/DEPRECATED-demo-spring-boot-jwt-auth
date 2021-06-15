f2a24e50c9237ad877c3c0df05d7552b5f823b88
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2021-06-15 06:20:30 +0000

    @Table annotation added, application-prod.properties added

--- src/main/java/com/example/demo/security/User.java
+++ src/main/java/com/example/demo/security/User.java
@@ -10,6 +10,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -26,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 @Entity
 @RequiredArgsConstructor
 @NoArgsConstructor
+@Table(name = "user")
 public class User implements UserDetails {
 
 	private static final long serialVersionUID = 1L;
